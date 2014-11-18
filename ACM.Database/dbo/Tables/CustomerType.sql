49b6ac3d783515dd2fc0feb1cb11941c2007257c
Deborah Kurata <deborahk@insteptech.com>
2014-11-17 20:02:08 -0500

    Modified the database project.

---
+++ ACM.Database/dbo/Tables/CustomerType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CustomerType] (
+    [CustomerTypeId] INT           NOT NULL IDENTITY,
+    [TypeName]    NVARCHAR (50) NULL,
+    [IsSystem] BIT NULL, 
+    CONSTRAINT [PK_CustomerType] PRIMARY KEY ([CustomerTypeId]) 
+);
+
