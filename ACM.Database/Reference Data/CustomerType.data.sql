dccc952e6bf014fdbbf065cfd5427d6059f63752
Deborah Kurata <deborahk@insteptech.com>
2014-11-17 19:47:26 -0500

    Changed the CustomerType data script from Inserts to a Merge script.

--- ACM.Database/Reference Data/CustomerType.data.sql
+++ ACM.Database/Reference Data/CustomerType.data.sql
@@ -1,8 +1,37 @@
-﻿DELETE FROM CustomerType
+﻿– Reference Data for the CustomerType table 
+SET IDENTITY_INSERT [CustomerType] ON 
+  
+MERGE INTO [CustomerType] AS Target 
+USING (VALUES 
+            (1, N’Corporation’, 2), 
+            (2, N’Individual’, 1), 
+            (3, N’Educator’, 3), 
+            (4, N’Government’, 4)) 
+AS Source ([CustomerTypeId], 
+            [TypeName], 
+            [DisplayOrder]) 
+ON (Target.[CustomerTypeId] = Source.[CustomerTypeId])
 
-SET IDENTITY_INSERT [dbo].[CustomerType] ON
-INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [TypeName]) VALUES (1, N'Corporation')
-INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [TypeName]) VALUES (2, N'Individual')
-INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [TypeName]) VALUES (3, N'Educator')
-INSERT INTO [dbo].[CustomerType] ([CustomerTypeId], [TypeName]) VALUES (4, N'Government')
-SET IDENTITY_INSERT [dbo].[CustomerType] OFF
+WHEN MATCHED AND 
+            (Target.[TypeName] <> Source.[TypeName]) THEN 
+UPDATE SET 
+            [TypeName] = Source.[TypeName], 
+            [DisplayOrder] = Source.[DisplayOrder]
+
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT([CustomerTypeId], 
+            [TypeName], 
+            [DisplayOrder]) 
+VALUES(Source.[CustomerTypeId], 
+        Source.[TypeName], 
+        Source.[DisplayOrder])
+
+– delete rows that are in the target but not the source 
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE; 
+  
+GO 
+  
+SET IDENTITY_INSERT [CustomerType] OFF 
+GO
+
